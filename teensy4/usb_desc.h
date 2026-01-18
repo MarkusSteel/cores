172bdc2534662b6b8c2f66c44c2a364b97ce23f8
Markus <markus@stahlbergs.de>
2026-01-18 19:06:20 +0100

    Add new USB configuration

--- teensy4/usb_desc.h
+++ teensy4/usb_desc.h
@@ -853,6 +853,62 @@ let me know?  http://forum.pjrc.com/forums/4-Suggestions-amp-Bug-Reports
   #define ENDPOINT5_CONFIG	ENDPOINT_RECEIVE_ISOCHRONOUS + ENDPOINT_TRANSMIT_ISOCHRONOUS
   #define ENDPOINT6_CONFIG	ENDPOINT_RECEIVE_UNUSED + ENDPOINT_TRANSMIT_ISOCHRONOUS
 
+#elif defined(USB_MIDI2_AUDIO_RAWHID)
+  #define VENDOR_ID		0x16C0
+  #define PRODUCT_ID		0x0486
+  #define RAWHID_USAGE_PAGE	0xFFAB  // recommended: 0xFF00 to 0xFFFF
+  #define RAWHID_USAGE		0x0200  // recommended: 0x0100 to 0xFFFF
+  //#define VENDOR_ID		        0x16C0
+  //#define PRODUCT_ID		    0x0333
+  //#define RAWHID_USAGE_PAGE	    0xFF33  // recommended: 0xFF00 to 0xFFFF
+  //#define RAWHID_USAGE		    0x0333  // recommended: 0x0100 to 0xFFFF
+  #define MANUFACTURER_NAME	    {'T','e','e','n','s','y','d','u','i','n','o'}
+  #define MANUFACTURER_NAME_LEN	11
+  #define PRODUCT_NAME		    {'T','e','e','n','s','y',' ','M','I','D','I','x','1','6','/','A','u','d','i','o'}
+  #define PRODUCT_NAME_LEN	    20
+  #define EP0_SIZE		        64
+  #define NUM_ENDPOINTS         9
+  #define NUM_INTERFACE		    6
+  
+  #define RAWHID_INTERFACE      0	// RawHID
+  #define RAWHID_TX_ENDPOINT    3
+  #define RAWHID_TX_SIZE        64
+  #define RAWHID_TX_INTERVAL    1	 // TODO: is this ok for 480 Mbit speed
+  #define RAWHID_RX_ENDPOINT    4
+  #define RAWHID_RX_SIZE        64
+  #define RAWHID_RX_INTERVAL    1	 // TODO: is this ok for 480 Mbit speed
+
+  #define SEREMU_INTERFACE      1	// Serial emulation
+  #define SEREMU_TX_ENDPOINT    2
+  #define SEREMU_TX_SIZE        64
+  #define SEREMU_TX_INTERVAL    1	 // TODO: is this ok for 480 Mbit speed
+  #define SEREMU_RX_ENDPOINT    2
+  #define SEREMU_RX_SIZE        32
+  #define SEREMU_RX_INTERVAL    2	 // TODO: is this ok for 480 Mbit speed
+  
+  #define MIDI_INTERFACE        2	// MIDI
+  #define MIDI_NUM_CABLES       2
+  #define MIDI_TX_ENDPOINT      5
+  #define MIDI_TX_SIZE_12       64
+  #define MIDI_TX_SIZE_480      512
+  #define MIDI_RX_ENDPOINT      5
+  #define MIDI_RX_SIZE_12       64
+  #define MIDI_RX_SIZE_480      512
+
+  #define AUDIO_INTERFACE	    3	// Audio (uses 3 consecutive interfaces)
+  #define AUDIO_TX_ENDPOINT     6
+  #define AUDIO_TX_SIZE         180
+  #define AUDIO_RX_ENDPOINT     6
+  #define AUDIO_RX_SIZE         180
+  #define AUDIO_SYNC_ENDPOINT	7
+
+  #define ENDPOINT2_CONFIG	ENDPOINT_RECEIVE_INTERRUPT + ENDPOINT_TRANSMIT_INTERRUPT
+  #define ENDPOINT3_CONFIG	ENDPOINT_RECEIVE_UNUSED + ENDPOINT_TRANSMIT_INTERRUPT
+  #define ENDPOINT4_CONFIG	ENDPOINT_RECEIVE_INTERRUPT + ENDPOINT_TRANSMIT_UNUSED
+  #define ENDPOINT5_CONFIG	ENDPOINT_RECEIVE_BULK + ENDPOINT_TRANSMIT_BULK
+  #define ENDPOINT6_CONFIG	ENDPOINT_RECEIVE_ISOCHRONOUS + ENDPOINT_TRANSMIT_ISOCHRONOUS
+  #define ENDPOINT7_CONFIG	ENDPOINT_RECEIVE_UNUSED + ENDPOINT_TRANSMIT_ISOCHRONOUS
+
 #elif defined(USB_EVERYTHING)
   #define VENDOR_ID		0x16C0
   #define PRODUCT_ID		0x0476
